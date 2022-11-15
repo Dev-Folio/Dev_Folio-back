46aab8f8f7d5b89aed61263e37811c4f81bda204
UniM0cha <solst_ice@naver.com>
2022-11-15 13:35:54 +0900

    1115 - 프로젝트 생성

---
+++ src/main/java/com/inhatc/dev_folio/DevFolioApplication.java
@@ -0,0 +1,13 @@
+package com.inhatc.dev_folio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevFolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inhatc/dev_folio/DevFolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inhatc.dev_folio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevFolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
