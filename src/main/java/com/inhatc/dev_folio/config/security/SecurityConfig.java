0cd266df1dabee6fb5941bfe743d575895df196f
UniM0cha <solst_ice@naver.com>
2022-11-18 16:13:35 +0900

    1118 - 시큐리티 설정 추가

---
+++ src/main/java/com/inhatc/dev_folio/config/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.inhatc.dev_folio.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests()
+                .mvcMatchers("/css/**", "/js/**").permitAll()
+                .mvcMatchers("/", "/member/**", "/item/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
